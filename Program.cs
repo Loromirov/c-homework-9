68cdd114134b5142e00281a037ee2b236b159511
Loromirov <loromirov@yandex.ru>
2022-12-25 22:58:46 +0300

    третья задача

--- Program.cs
+++ Program.cs
@@ -1,32 +1,34 @@
 ﻿/*
 
-Задача 66: Задайте значения M и N. Напишите программу, 
-которая найдёт сумму натуральных элементов в промежутке от M до N.
-
-M = 1; N = 15 -> 120
-M = 4; N = 8. -> 30
-
+Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29
 */
 
+Console.Write("Введите число M: ");
+int m = int.Parse(Console.ReadLine());
 
-int InputInt(string output)
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+AkkermanFunction(m, n);
+
+void AkkermanFunction(int m, int n)
 {
-    Console.Write(output);
-    return int.Parse(Console.ReadLine());
+    System.Console.Write(Akker(m, n));
 }
 
-int CountNaturalSum(int m, int n)
+int Akker(int m, int n)
 {
-    if (m == n)
+    if (m == 0)
     {
-        return n;
+        return n + 1;
+    }
+    else if (m > 0 && n == 0)
+    {
+        return Akker(m -1, 1);
     }
     else
     {
-        return n + CountNaturalSum(m, n - 1);
+        return (Akker(m - 1, Akker(m, n - 1)));
     }
 }
-
-int m = InputInt("Введите M:");
-int n = InputInt("Введите N:");
-Console.WriteLine(CountNaturalSum(m, n));
